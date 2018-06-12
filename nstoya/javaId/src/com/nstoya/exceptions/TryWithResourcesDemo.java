5f7b5121cfde282f502ab29243933d63c22f20b8
Nadezhda bremmekamp <nadezhda@bremmekamp.com>
2018-06-12 08:38:48 +0200

    handling suppressed exceptions

--- nstoya/javaId/src/com/nstoya/exceptions/TryWithResourcesDemo.java
+++ nstoya/javaId/src/com/nstoya/exceptions/TryWithResourcesDemo.java
@@ -7,8 +7,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class TryWithResourcesDemo {
-	static String inFileStr = "walden.jpg";
-	static String outFileStr = "walden-out.jpg";
+	static String inFileStr = "Understanding-Method-Binding-In-Java.pdf";
+	static String outFileStr = "Understanding-Method-Binding-In-Java_out.pdf";
 		
 	public static void fileCopyWithArm() throws IOException {
 		System.out.println("\nInside fileCopyWithArm ...");				
@@ -25,9 +25,150 @@ public class TryWithResourcesDemo {
 		}		
 	}	
 	
+	public static void fileCopyWithoutArm() throws IOException {
+		System.out.println("\nInside fileCopyWithoutArm ...");
+		
+		Test t = null;
+		Test2 t2 = null;
+		BufferedInputStream in = null;
+		BufferedOutputStream out = null;
+		
+		try {
+			t = new Test();
+			t2 = new Test2();
+			in = new BufferedInputStream(new FileInputStream(inFileStr));
+			out = new BufferedOutputStream(new FileOutputStream(outFileStr));
+			
+			byte[] byteBuf = new byte[4000];
+			int numBytesRead;
+			while ((numBytesRead = in.read(byteBuf)) != -1) {
+				out.write(byteBuf, 0, numBytesRead);
+			}
+			
+			throw new IOException("Important Exception!!");
+			
+		} finally {
+			
+			if (t2 != null) 
+				t2.close();
+			
+			if (t != null) 
+				t.close();			
+			
+			if (in != null)
+				in.close();
+			
+			if (out != null)
+				out.close();
+		}		
+				
+	}
+	
+	// Handles Exception Masking via Suppression
+	public static void fileCopyWithoutArm2() throws IOException {
+		System.out.println("\nInside fileCopyWithoutArm2 ...");
+		
+		Test t = null;
+		Test2 t2 = null;
+		BufferedInputStream in = null;
+		BufferedOutputStream out = null;
+		
+		IOException ioException = null;		
+				
+		try {
+			t = new Test();
+			t2 = new Test2();
+			in = new BufferedInputStream(new FileInputStream(inFileStr));
+			out = new BufferedOutputStream(new FileOutputStream(outFileStr));
+			
+			byte[] byteBuf = new byte[4000];
+			int numBytesRead;
+			while ((numBytesRead = in.read(byteBuf)) != -1) {
+				out.write(byteBuf, 0, numBytesRead);
+			}
+			
+			throw new IOException("Important Exception!!");
+			
+		} catch (IOException e) {
+			ioException = e;
+		} finally {			
+			if (ioException != null) {
+				try {
+					if (t2 != null) {
+						t2.close();
+					}
+				} catch (IOException e) {
+					ioException.addSuppressed(e);					
+				}				
+			} else {
+				try {
+					if (t2 != null) {
+						t2.close();
+					}
+				} catch (IOException e) {
+					ioException = e;					
+				}
+			}
+			
+			if (ioException != null) {
+				try {
+					if (t != null) {
+						t.close();
+					}
+				} catch (IOException e) {
+					ioException.addSuppressed(e);					
+				}				
+			} else {
+				try {
+					if (t != null) {
+						t.close();
+					}
+				} catch (IOException e) {
+					ioException = e;					
+				}
+			}
+			
+			if (ioException != null) {
+				try {
+					if (in != null) {
+						in.close();
+					}
+				} catch (IOException e) {
+					ioException.addSuppressed(e);					
+				}				
+			} else {
+				try {
+					if (in != null) {
+						in.close();
+					}
+				} catch (IOException e) {
+					ioException = e;					
+				}
+			}
+			
+			if (ioException != null) {
+				try {
+					if (out != null) {
+						out.close();
+					}
+				} catch (IOException e) {
+					ioException.addSuppressed(e);					
+				}				
+				
+				throw ioException;
+			} else {
+				if (out != null) {
+					out.close();
+				}				
+			}
+		}		
+				
+	}
+	
 	public static void main(String[] args) {
 		try {
-			fileCopyWithArm();
+			fileCopyWithoutArm2();
+			//fileCopyWithoutArm2();
 		} catch (IOException e) {
 			e.printStackTrace();
 			
@@ -44,7 +185,6 @@ public class TryWithResourcesDemo {
 class Test implements AutoCloseable {
 	@Override
 	public void close() throws IOException {
-		//thrown as suppressed exception via the resource 
 		throw new IOException("Trivial Exception");			
 	}		
 }
@@ -52,7 +192,6 @@ class Test implements AutoCloseable {
 class Test2 implements AutoCloseable {
 	@Override
 	public void close() throws IOException {
-		//thrown as suppressed exception via the resource
 		throw new IOException("Trivial Exception 2");			
 	}		
 }
